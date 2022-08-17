89ca7acd6dd9852dfb4ca4164e40f06f9d91d00e
RahulKarki98 <karkirahul1999@gmail.com>
2022-08-17 10:27:12 +0530

    17 changes

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,12 +1,22 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Movie } from "./Movie";
+import { API } from "./global";
 
-export function MovieList({ movieList, setMovieList }) {
+export function MovieList() {
   // const movieList = INTIAL_MOVIE_LIST;
-  // const [movieList, setMovieList] = useState(INTIAL_MOVIE_LIST)
+  const [movieList, setMovieList] = useState([]);
   
+  useEffect(() => {
+    fetch(`${API}/movie`,{
+      method: "GET",
+    })
+    .then((data) => data.json())
+    .then((mvs) =>setMovieList(mvs));
+   },[]);
+
+
   return (
     <div>
       
